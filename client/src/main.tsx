3ed4025e9e096f4ef7594107b3dee244ee66eff9
Cami C.A <camic.a@Cami-6.local>
2024-09-28 14:54:17 -0400

    added routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Login from './Login/Login.tsx'
+import Dashboard from './Dashboard/Dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
