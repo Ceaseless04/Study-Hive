e55ad57887c25bb9e7ba92525ab5f016a5145538
Ceaseless04 <kristian120304@gmail.com>
2024-09-28 16:32:02 -0400

    WIP, got flashcards API to work

--- backend/server/app.py
+++ backend/server/app.py
@@ -43,7 +43,11 @@ async def get_flashcards(university: str, course: str):
         
         # Send the prompt to the model
         response = model.generate_content(
+<<<<<<< HEAD
             f"Based on the topics {topics} from the course {course} at this school {university}, can you quiz me on the topics provided? Make sure the University is a Valid University",
+=======
+            f"Based on the topics {topics} from the course {course} at this school {university}, can you quiz me on the topics provided?",
+>>>>>>> 56af586 (WIP, got flashcards API to work)
             generation_config=genai.GenerationConfig(
                 response_mime_type="application/json"  # Keep the response type to JSON
             )
