79295b78f05d2e245dcaca55b2b409540ff0f8f5
Jose Amiot <jmamiot04@gmail.com>
2024-09-28 14:56:42 -0400

    woring on signup

---
+++ client/src/Signup/Signup.tsx
@@ -0,0 +1,46 @@
+import './Signup.css'
+
+const Signup = () => {
+    return (
+  
+      
+      <div className='wrapper'> 
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box" style={{ display: 'flex', alignItems: 'center' }}>
+          <span style={{ marginRight: '8px' }}>
+            {/* <FaUser /> */}
+          </span>
+          <input type="text" placeholder="Username" required />
+        </div>
+        
+        <div className="input-box" style={{ display: 'flex', alignItems: 'center' }}>
+            <span style={{ marginRight: '8px' }}>
+              {/* <FaLock /> */}
+            </span>
+            <input type="password" placeholder='Password' required/>
+            
+  
+  
+        </div>
+  
+        <div className="remember-forgotten">
+          <label><input type="checkbox" />Remember me</label>
+          <a href="#">Forgot password?</a>
+        </div>
+  
+        <button type="submit">Login</button>
+  
+        <div className="register-link">
+  
+          <p>Do not have an account? <a href="#">Register</a></p>
+        </div>
+      </form>
+      
+      </div>
+      
+    )
+  }
+  
+  export default Signup
+  
