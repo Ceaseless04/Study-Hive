6411a1648ba7acc459e124076535b85c4109e959
Cami C.A <camic.a@Cami-6.local>
2024-09-29 09:14:34 -0400

    working to push from main

--- client/src/Dashboard/Dashboard.tsx
+++ client/src/Dashboard/Dashboard.tsx
@@ -1,16 +1,29 @@
 import './Dashboard.css'
 import 'react-icons/fa';
 import DashNavbar from '../Components/navbar';
+import { Link } from 'react-router-dom';
 
 
 const Dashboard = () => {
   return (
-    <div className='page_container'>
+    <div className='home-page'>
+        <DashNavbar/>
+
         <div>
             <h1>Welcome to Study Hive</h1>
-            <DashNavbar/>
+        </div>
 
-            
+        <div className="intro">
+            <h2 id="title">Your AI Study Companion and Helper</h2>
+            <p id="description">
+            Have Flashcard decks based on your university <br/>
+            and class in seconds. Save them and jump to Studying!
+            </p>
+            <Link to="/Signup" className="link">
+              <button className="sign-up-button">
+                <span>Start my Journey</span>
+              </button>
+            </Link>
         </div>
 
     </div>
